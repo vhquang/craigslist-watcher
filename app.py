f5fb267242a673eed7abca56c622b52ede9f40b3
Quang Vu <quang@keypr.com>
2016-07-30 15:11:22 -0700

    add item description

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory, jsonify, request
+from pprint import pprint
 import dateutil.parser
+import json
 import redis
+import requests
+import validators
 
-EXPIRE_TIME = 3600 * 24 * 7  # seconds
+
+REDIS_ITEM_EXPIRE_TIME = 3600 * 24 * 7  # seconds
+CLOJURE_APP = 'http://localhost:3000'
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -22,9 +28,30 @@ def archive_item_redis(item_id):
     return redis_db.renamenx(new_item_key, old_item_key)
 
 
+def update_database(data):
+    redis_db = redis.StrictRedis(host='localhost', port=6379, db=0)
+    for _id, item in data.iteritems():
+        # pprint(item)
+        key_old_item = 'old:item:{}'.format(_id)
+        key_new_item = 'new:item:{}'.format(_id)
+        if item['price'] != redis_db.hget(key_old_item, 'price'):
+            redis_db.delete(key_old_item)
+            redis_db.hmset(key_new_item, item)
+            redis_db.expire(key_new_item, REDIS_ITEM_EXPIRE_TIME)
+            # gcm_notify(DEVICE_TOKEN, item['title'])
+
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+# @app.route('/test')
+# def delay():
+#     page = int(request.args.get('page', 1))
+#     print '----------', page
+#     time.sleep(1)
+#     res = [] if page == 10 else [page]
+#     return json.dumps(res)
 
 
 @app.route('/<path>')
@@ -38,6 +65,18 @@ def get_new_items():
                        key=lambda x: dateutil.parser.parse(x['time']),
                        reverse=True)
     return jsonify({'items': new_items})
+
+
+@app.route('/api/scrape-link', methods=['POST'])
+def retrieve_items():
+    query_link = request.get_json().get('link')
+    if not validators.url(query_link):
+        return jsonify({"error": "Not valid link"}), 400
+    resp = requests.post(CLOJURE_APP + '/scrape', headers={"content-type": "application/json"},
+                         json={"link": query_link})
+    data = json.loads(resp.text)
+    update_database(data)
+    return jsonify({}), 204
 
 
 @app.route('/api/item/<item_id>/archive', methods=['POST'])
--- static/app.js
+++ static/app.js
@@ -25,15 +25,20 @@ angular
 
 angular.module('yeomanApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/new-item').then(
-      function(result) {
-        $scope.itemList = result['data']['items'] || [];
-        // console.log(result);
-      },
-      function(err) {
-        console.error(err);
-      }
-    )
+
+    function getItems() {
+      $http.get('/api/new-item').then(
+        function (result) {
+          $scope.itemList = result['data']['items'] || [];
+          // console.log(result);
+        },
+        function (err) {
+          console.error(err);
+        }
+      );
+    }
+
+    getItems();
 
     $scope.archive = function(id) {
       $http.post('/api/item/'+ id +'/archive').then(
@@ -42,4 +47,17 @@ angular.module('yeomanApp')
           _.remove($scope.itemList, function(n) { return n['id'] == id; });
         });
     };
+
+    $scope.targetLink = '';
+    $scope.scrapeLink = function(link) {
+      if (!link) { return; }
+      $scope.isLoading = true;
+      $http.post('/api/scrape-link', {"link": link}).then(
+        function(resp) { getItems(); },
+        function (err) { console.error(err); },
+        function (progress) { console.log(progress); }
+      ).finally(
+        function() { $scope.isLoading = false; }
+      );
+    }
   });
