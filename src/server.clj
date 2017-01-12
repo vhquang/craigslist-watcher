a40d75f736a731cf2c3bc883085505ff5f86b654
Quang Vu <vhquang@gmail.com>
2017-01-11 16:27:48 -0800

    Setup deploy (#4)
    * replace compojure.handler with ring.middleware.defaults
    
    * update to compojure 1.5.2, set main class
    
    * add ring-jetty for running server
    
    * use port from environment variable
    
    * refactor

--- src/server.clj
+++ src/server.clj
@@ -1,8 +1,10 @@
 (ns server
+  (:gen-class)
   (:require [compojure.core :refer :all]
-            [compojure.handler :as handler]
             [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [ring.adapter.jetty :refer [run-jetty]]
             [scrapper]))
 
 (def test-link "https://losangeles.craigslist.org/search/sss?query=*&sort=date&min_price=700&max_price=700")
@@ -19,9 +21,11 @@
   (route/resources "/")
   (route/not-found "Page not found"))
 
-;(def route-handler (handler/site app-routes))
-
 (def app
-  (-> (handler/site app-routes)
+  (-> (wrap-defaults app-routes site-defaults)
       (wrap-json-body {:keywords? true})
       (wrap-json-response)))
+
+(defn -main [& args]
+  (let [port (Integer/parseInt (get (System/getenv) "APP_PORT" "8080"))]
+    (run-jetty app {:port port})))
